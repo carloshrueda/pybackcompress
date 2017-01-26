aecbb9153f8fefc2ea60d91c776595796775ff0b
crueda <carloshrueda@gmail.com>
2017-01-26 13:54:38 -0500

    pybackcompress: -Agregaron más argumentos
    test.py:
    se creo archivo para pruebas temporales

--- pybackcompress.py
+++ pybackcompress.py
@@ -1,31 +1,60 @@
+#! python
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
 
 
 def main(args):
     # Aquí procesamos lo que se tiene que hacer con cada argumento
-    if args.verbose:
-        print("depuración activada!!!", args.verbose)
-
-    if args.file:
-        print("El nombre de archivo a procesar es: ", args.file)
+    print("Argumento: ", args)
 
 
-def addArgumentos(p):
-    p.add_argument("-d", help="Ruta de destino")
-    p.add_argument("-ex", help="[-ex archivo] Archivo con extensiones para excluir")
-    p.add_argument("-mt", help="[-h num] Numero de hilos")
-    p.add_argument("-n", help="[-n nivel(0..9)] nivel de compresion")
-    p.add_argument("-o", help="Ruta de origen")
-    p.add_argument("-t", help="[-t tipo_compress (7z, zip, bzip2)] Tipo de compresion")
-    p.add_argument("-u", help="[-u on | off] Actualizar")
-    p.add_argument("-v", help="Mostrar información de depuración", action="store_true")
+def getargumentos(ha):
+    parser = argparse.ArgumentParser(description='Haciendo backup con compresión', usage="%(prog)s [opciones]",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("o", help="ruta de origen")
+    parser.add_argument("d", help="ruta de destino")
+
+    parser.add_argument("-a", choices=['on', 'off'], help="actualizar archivos (por de defecto: %(default)s)",
+                        default="on")
+    parser.add_argument("-e", type=str, help="ruta del archivo con extensiones para excluir")
+    parser.add_argument("-l", type=str, choices=['on', 'off'], help="log (por de defecto: %(default)s)", default="on")
+    parser.add_argument("-nc", type=int, choices="09", help="nivel de compresion (por de defecto: %(default)s)",
+                        default=9)
+    parser.add_argument("-nh", type=int, help="número de hilos (por de defecto: %(default)s)", default=1)
+    parser.add_argument("-t", type=str, choices=['7z', 'bzip2', 'zip'],
+                        help="tipo de compresion (por de defecto: %(default)s)", default="zip")
+    parser.add_argument("-dl", type=str, help="ruta destino log (por de defecto: %(default)s)", default=".")
+    parser.add_argument("--verbose", help="mostrar información de depuración", default=argparse.SUPPRESS)
+    parser.add_argument('-v', action='version', version='%(prog)s - Version 0.1', help="versión del programa",
+                        default=argparse.SUPPRESS)
+    ha = parser.parse_args()
+
+    # valildar rutas de origen
+    if not os.path.isdir(ha.o):
+        print("Error. La ruta de ORIGEN: " + ha.o + " no existe.")
+        ha = None
+        return
+
+    if not os.path.isdir(ha.d):
+        print("Error. La ruta de DESTINO: " + ha.d + " no existe.")
+        ha = None
+        return
+
+    if ha.l and not os.path.isdir(ha.dl):
+        print("Error. La ruta del LOG: " + ha.dl + " no existe.")
+        ha = None
+        return
+
+    if ha.e is not None and not os.path.isdir(ha.e):
+        print("Error. La ruta del archivo para EXCLUIR: " + ha.e + " no existe.")
+        ha = None
+        return
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    addArgumentos(parser)
-    args = parser.parse_args()
-
-    main(args)
+    hargs = None
+    getargumentos(hargs)
+    if hargs:
+        main(hargs)
