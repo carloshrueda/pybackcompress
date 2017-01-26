32935c54deebcb1a380623ec2fbae2c009fd4d0e
Carlos Rueda <carloshrueda>
2017-01-25 21:39:20 -0500

    se creo la funcion main

--- pybackcompress.py
+++ pybackcompress.py
@@ -2,14 +2,19 @@
 
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="Mostrar información de depuración", action="store_true")
-parser.add_argument("-f", "--file", help="Nombre de archivo a procesar")
-args = parser.parse_args()
+def main(args):
+    # Aquí procesamos lo que se tiene que hacer con cada argumento
+    if args.verbose:
+        print("depuración activada!!!", args.verbose)
 
-# Aquí procesamos lo que se tiene que hacer con cada argumento
-if args.verbose:
-    print("depuración activada!!!", args.verbose)
+    if args.file:
+        print("El nombre de archivo a procesar es: ", args.file)
 
-if args.file:
-    print("El nombre de archivo a procesar es: ", args.file)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="Mostrar información de depuración", action="store_true")
+    parser.add_argument("-f", "--file", help="Nombre de archivo a procesar")
+    args = parser.parse_args()
+
+    main(args)
+
